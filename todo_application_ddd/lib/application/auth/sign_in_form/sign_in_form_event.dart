c5938c6fbf692207208d7b036bf5dfae1aacb49a
amitsingh6391 <amitsingh506142@gmail.com>
2021-03-11 15:09:16 +0530

    part 4  Modeling the Sign-In Form Events & State

---
+++ todo_application_ddd/lib/application/auth/sign_in_form/sign_in_form_event.dart
@@ -0,0 +1,15 @@
+part of 'sign_in_form_bloc.dart';
+
+@freezed
+abstract class SignInFormEvent with _$SignInFormEvent {
+  // Notice that these events take in "raw" unvalidated Strings
+  const factory SignInFormEvent.emailChanged(String emailStr) = EmailChanged;
+  const factory SignInFormEvent.passwordChanged(String passwordStr) =
+      PasswordChanged;
+  const factory SignInFormEvent.registerWithEmailAndPasswordPressed() =
+      RegisterWithEmailAndPasswordPressed;
+  const factory SignInFormEvent.signInWithEmailAndPasswordPressed() =
+      SignInWithEmailAndPasswordPressed;
+  const factory SignInFormEvent.signInWithGooglePressed() =
+      SignInWithGooglePressed;
+}
