ade069a5be8718a9da071f6cabb0c70d0bc1367f
amitsingh6391 <amitsingh506142@gmail.com>
2021-03-12 13:44:02 +0530

    part 4 sign in form ui

--- todo_application_ddd/lib/main.dart
+++ todo_application_ddd/lib/main.dart
@@ -1,27 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:todo_application_ddd/injection.dart';
+import 'package:todo_application_ddd/presentation/core/app_widget.dart';
 
 void main() {
   configureInjection(Environment.prod);
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material app Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Todo appp DDD'),
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(AppWidget());
 }
---
+++ todo_application_ddd/lib/presentation/sign_in/sign_in_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_application_ddd/application/auth/sign_in_form/sign_in_form_bloc.dart';
+import 'package:todo_application_ddd/injection.dart';
+import 'package:todo_application_ddd/presentation/sign_in/widgets/sign_in_form.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: BlocProvider(
+        create: (context) => getIt<SignInFormBloc>(),
+        child: SignInForm(),
+      ),
+    );
+  }
+}
