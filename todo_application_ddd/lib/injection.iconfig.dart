97dcb73fa8e47a4085c7e09a79967b59a61633a3
amitsingh6391 <amitsingh506142@gmail.com>
2021-03-14 13:39:31 +0530

    part 20 placeholder note bloc

--- todo_application_ddd/lib/injection.iconfig.dart
+++ todo_application_ddd/lib/injection.iconfig.dart
@@ -4,6 +4,8 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:todo_application_ddd/application/notes/note_form/note_form_bloc.dart';
 import 'package:todo_application_ddd/infrastructure/core/firebase_injectable_module.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -15,16 +17,21 @@ import 'package:todo_application_ddd/application/notes/note_watcher/note_watcher
 import 'package:todo_application_ddd/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:todo_application_ddd/application/auth/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:todo_application_ddd/infrastructure/notes/note_repository.dart';
 
 void $initGetIt(GetIt g, {String environment}) {
   final firebaseInjectableModule = _$FirebaseInjectableModule();
   g.registerLazySingleton<FirebaseAuth>(
       () => firebaseInjectableModule.firebaseAuth);
+  g.registerLazySingleton<Firestore>(() => firebaseInjectableModule.firestore);
   g.registerLazySingleton<GoogleSignIn>(
       () => firebaseInjectableModule.googleSignIn);
   g.registerLazySingleton<IAuthFacade>(
       () => FirebaseAuthFacade(g<FirebaseAuth>(), g<GoogleSignIn>()));
+  g.registerLazySingleton<INoteRepository>(
+      () => NoteRepository(g<Firestore>()));
   g.registerFactory<NoteActorBloc>(() => NoteActorBloc(g<INoteRepository>()));
+  g.registerFactory<NoteFormBloc>(() => NoteFormBloc(g<INoteRepository>()));
   g.registerFactory<NoteWatcherBloc>(
       () => NoteWatcherBloc(g<INoteRepository>()));
   g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
