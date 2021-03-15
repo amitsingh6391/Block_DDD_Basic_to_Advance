f543c86cfff5ec33979e6882bb265845efa391da
amitsingh6391 <amitsingh506142@gmail.com>
2021-03-15 08:13:08 +0530

    part 29 todo_list widget

--- todo_application_ddd/lib/presentation/notes/note_form/note_form_page.dart
+++ todo_application_ddd/lib/presentation/notes/note_form/note_form_page.dart
@@ -11,6 +11,7 @@ import 'package:todo_application_ddd/presentation/notes/note_form/misc/todo_item
 import 'package:todo_application_ddd/presentation/notes/note_form/widgets/add_todo_tile_widget.dart';
 import 'package:todo_application_ddd/presentation/notes/note_form/widgets/body_field_widget.dart';
 import 'package:todo_application_ddd/presentation/notes/note_form/widgets/color_field_widget.dart';
+import 'package:todo_application_ddd/presentation/notes/note_form/widgets/todo_list_widget.dart';
 import 'package:todo_application_ddd/presentation/routes/router.gr.dart';
 import 'package:provider/provider.dart';
 
@@ -145,6 +146,7 @@ class NoteFormPageScaffold extends StatelessWidget {
                   children: [
                     const BodyField(),
                     const ColorField(),
+                    const TodoList(),
                     const AddTodoTile(),
                   ],
                 ),
